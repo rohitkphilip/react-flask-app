9e3b6ceee5bd0334af9fbcf6287ea87e693f79fc
rohitkphilip <rohitkurian.philip@sjsu.edu>
2024-04-21 15:36:48 -0700

    intial commit 2

---
+++ backend/main.py
@@ -0,0 +1,74 @@
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+
+
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = list(map(lambda x : x.to_json(), contacts))
+    return jsonify({"contacts": json_contacts})
+
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+
+    if not first_name or not last_name or not email:
+        return (
+            jsonify({"message": "You must include a first name, last name, and email"}),
+            400,
+        )
+    
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+
+    return jsonify({"message": "Contact created!"}), 201
+
+
+@app.route("/update_contact/<int:contact_id>", methods=["PATCH"])
+def update_contact(contact_id):
+    contact = Contact.query.get(contact_id)
+
+    if not contact:
+        return jsonify({"message": "Contact not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name) # if firstName exists in data, it returns that value. else, it returns contact.first_name(original first name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "Contact updated."}), 201
+
+
+
+@app.route("/delete_contact/<int:contact_id>", methods=["DELETE"])
+def delete_contact(contact_id):
+    contact = Contact.query.get(contact_id)
+
+    if not contact:
+        return jsonify({"message": "Contact not found"}), 404
+    
+    db.session.delete(contact)
+    db.session.commit()
+
+    return jsonify({"message": "Contact Deleted"}), 200
+
+
+
+if __name__ == "__main__": # the code within this if block is only run when this file is executed as a script. but not when it is imported as a module
+
+    with app.app_context():
+        db.create_all()  # check if database has been created. if not, it creates the database
+
+    app.run(debug=True)
